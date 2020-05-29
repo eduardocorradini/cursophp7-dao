d6477da9655e61917ca13ef05d5d0e2ce652547b
Eduardo Corradini <eduardo_corradini@yahoo.com.br>
2020-05-29 10:56:55 -0300

    atualizacao usuario

--- class/Usuario.php
+++ class/Usuario.php
@@ -46,6 +46,39 @@ class Usuario {
 			$this->setDtcadastro(new DateTime($row['dtcadastro']));
 		}
 	}
+	
+	public static function getList(){
+		$sql = new Sql;
+		return $sql->select("SELECT * FROM tb_usuarios order by deslogin");
+	}
+	
+	public static function search($login){
+		$sql = new Sql;
+		return $sql->select("SELECT * FROM tb_usuarios where deslogin like :SEARCH order by deslogin",array(
+			':SEARCH'=>"%".$login."%"
+		));
+	}
+	
+	public function login($login,$password){
+		$sql = new Sql;
+		$results = $sql->select("SELECT * FROM tb_usuarios where deslogin = :LOGIN AND dessenha = :PASSWORD",array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+		
+		if(count($results) > 0){
+			
+			$row = $results[0];
+			
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+		}else{
+			throw new exception("Login e/ou senha invalidos");
+		}
+	}
+	
 	public function __toString(){
 		return json_encode(array(
 			"idusuario" => $this->getIdusuario(),
--- index.php
+++ index.php
@@ -5,9 +5,23 @@ require_once("config.php");
 //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 //echo json_encode($usuarios);
 
+//GET ONLY 1 USUARIO
 $user = new Usuario();
 $user->loadById(3);
+//echo $user;
 
-echo $user;
+//GET LIST USUARIO
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Localizar login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Carrega um usuario com login e senha
+$usuario = new Usuario();
+$usuario->login("edu","1234");
+
+echo $usuario;
 
 ?>
